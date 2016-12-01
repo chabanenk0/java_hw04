86558efbde64e26c3c5210defdd96dc095c124c5
Dmytro Chabanenko <chdn6026@mail.ru>
2016-12-01 10:08:44 +0200

    Implemented zip service

--- src/main/java/com/github/chabanenk0/Main.java
+++ src/main/java/com/github/chabanenk0/Main.java
@@ -2,8 +2,10 @@ package com.github.chabanenk0;
 
 import com.github.chabanenk0.Entity.FileInformation;
 import com.github.chabanenk0.Exception.WrongPathException;
+import com.github.chabanenk0.Service.ArchiveBuilderService;
 import com.github.chabanenk0.Service.FileFinderService;
 
+import java.io.IOException;
 import java.util.Collection;
 
 /**
@@ -14,12 +16,17 @@ public class Main
     public static void main(String[] args) {
 
         FileFinderService fileFinderService = new FileFinderService();
-        Collection<FileInformation> res = null;
+        ArchiveBuilderService archiveBuilderService = new ArchiveBuilderService();
+        Collection<FileInformation> filesCollection = null;
         try {
-            res = fileFinderService.findFilesByType(".", "java");
+            filesCollection = fileFinderService.findFilesByType(".", "java");
+            archiveBuilderService.addFiles(filesCollection);
+            archiveBuilderService.pack("output.zip");
         } catch (WrongPathException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(res);
+
     }
 }
